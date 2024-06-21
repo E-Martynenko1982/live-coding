3fa8887f8e65269891caadff9e71c42e05c490d0
E-Martynenko1982 <motor8103@gmail.com>
2024-06-21 15:15:22 +0300

    reduce

--- callback/index2.js
+++ callback/index2.js
@@ -1,10 +1,20 @@
 // reduce input- callback output -anything
 //callback-input: acc,el,index,array, output-new acc
+
 function calcExpenses(transactions) {
+  //   const res = transactions.reduce((acc, el, index) => {
+  //     console.log('STEP' + index)
+  //     console.log('acc' + acc)
+  //     console.log('el' + el)
+  //     if (el < 0) {
+  //       acc += el
+  //     }
+  //   }, 0)
+
+  // return Math.abs(res)
+  return Math.abs(transactions.reduce((acc, el, index) => (el < 0 ? acc + el : acc), 0))
 
 
-
-  return Math.abs(transactions.reduce((acc, el) => (el < 0 ? acc + el : acc), 0))
 }
 
 // examples
