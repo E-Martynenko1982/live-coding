960a29583fce18e1674a0e9feae65bbc58dbf4b8
E-Martynenko1982 <motor8103@gmail.com>
2024-07-09 15:23:49 +0300

    update

--- classes/index.js
+++ classes/index.js
@@ -1,6 +1,8 @@
 
 import { Student } from "./student.js";
 import { User } from "./user.js";
+
+
 const getTopStudent = students => {
 
   const { id, firstName, lastName } = students
@@ -21,16 +23,25 @@ console.log(user1.getFullName());
 console.log(user2.getFullName());
 console.log(user3.getFullName());
 
-const student1 = new Student(1, 'Alice', 'Johnson', 'Math', 85)
-const student2 = new Student(2, 'Bob', 'Smith', 'Physicscons', 92);
-const student3 = new Student(3, 'Carol', 'Williams', 'Chemistry', 78);
+
+const student1 = new Student(1, 'Alice', 'Johnson', 'Math', 85);
+// const student2 = new Student(2, 'Bob', 'Smith', 'Physics', 92);
+const student3 = new Student(3, 'Carol', 'Williams', 'Chemistry', 178);
+
+const students = [student1, student3];
+
+console.log(getTopStudent(students))
+
+
+// const student2 = new Student(2, 'Bob', 'Smith', 'Physicscons', 92);
+// const student3 = new Student(3, 'Carol', 'Williams', 'Chemistry', 78);
 
 
 
-const students = [student1, student2, student3];
-const students1 = [student1, student2];
-const students2 = [student1, student3];
+// const students = [student1, student2, student3];
+// const students1 = [student1, student2];
+// const students2 = [student1, student3];
 
-console.log(getTopStudent(students));
-console.log(getTopStudent(students1));
-console.log(getTopStudent(students2));
+// console.log(getTopStudent(students));
+// console.log(getTopStudent(students1));
+// console.log(getTopStudent(students2));
