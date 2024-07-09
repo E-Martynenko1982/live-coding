8822da99a3512c5bc32ce2936fd0d876908a3ac9
E-Martynenko1982 <motor8103@gmail.com>
2024-07-09 15:02:07 +0300

    update

--- classes/index.js
+++ classes/index.js
@@ -14,6 +14,13 @@ const getTopStudent = students => {
   return { id, firstName, lastName, }
 };
 
+const user1 = new User(1, 'John', 'Doe');
+const user2 = new User(2, 'Jane', 'Doe');
+const user3 = new User(3, 'Jim', 'Beam');
+console.log(user1.getFullName());
+console.log(user2.getFullName());
+console.log(user3.getFullName());
+
 const student1 = new Student(1, 'Alice', 'Johnson', 'Math', 85)
 const student2 = new Student(2, 'Bob', 'Smith', 'Physicscons', 92);
 const student3 = new Student(3, 'Carol', 'Williams', 'Chemistry', 78);
